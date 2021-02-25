7427edcc71d2e36cfce5fc4c22dde9aac10ebfb6
Hugo Wood <hugo.wood@zenika.com>
2021-02-25 17:53:20 +0100

    🎨 make code compatible with node 12

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -25,7 +25,7 @@ export function httpRequest(url, options, body) {
   return new Promise((resolve, reject) => {
     const httpModule = url.toString().startsWith("https") ? https : http;
     const req = httpModule.request(url, effectiveOptions, async (res) => {
-      const statusCode = res.statusCode ?? 0;
+      const statusCode = res.statusCode || 0;
       log(`${req.method} ${url} => ${statusCode}`);
       if (statusCode >= 200 && statusCode < 400) {
         resolve(res);
