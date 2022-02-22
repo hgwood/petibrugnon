8c6ec2d2a16394a3dec589d507c369671379ffc3
Hugo Wood <hugo.wood@zenika.com>
2022-02-22 21:26:26 +0100

    🔥 remove commented code

--- src/hashCode.js
+++ src/hashCode.js
@@ -1,15 +1,4 @@
 export function findCurrentChallenge(adventures) {
-  // const editions = adventures.filter(
-  //   (adventure) => adventure.competition__str === "HASH_CODE"
-  // );
-  // return editions
-  //   .flatMap(({ challenges, ...edition }) =>
-  //     challenges.map((challenge) => ({ ...challenge, edition }))
-  //   )
-  //   .find(
-  //     ({ start_ms: startMs, end_ms: endMs }) =>
-  //       startMs < Date.now() && Date.now() < endMs
-  //   );
   return adventures
     .filter(({ competition__str }) => competition__str === "HASH_CODE")
     .flatMap(({ challenges }) => challenges)
