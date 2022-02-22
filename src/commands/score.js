e49ad3d6e24c4f28a44a36aa153c5dba828471f6
Hugo Wood <hugo.wood@zenika.com>
2022-02-22 21:40:29 +0100

    ♻ extract scoreboard transformation to scores
    Will be useful to write #13.

--- src/commands/score.js
+++ src/commands/score.js
@@ -1,20 +1,21 @@
 import { bold, blue } from "colorette";
 import { fetchScoreboard } from "../codeJamApiClient.js";
 import env from "../env.js";
+import { findScores } from "../hashCode.js";
 import { login } from "./login.js";
 
 export async function score() {
   const accessToken = await login();
   const scoreboard = await fetchScoreboard(env.meta.challengeId, accessToken);
-  const competitor = scoreboard.user_scores.find(
-    ({ competitor }) => competitor.id === env.meta.competitorId
+  const { teamName, totalScore, rank, tests } = findScores(
+    scoreboard,
+    env.meta.competitorId,
+    { testNames: env.meta.tests.map(({ name }) => name) }
   );
-  console.log(`Team: ${bold(blue(competitor.displayname))}`);
-  console.log(`Total Score: ${bold(blue(competitor.score_1))}`);
-  console.log(`Rank: ${bold(blue(competitor.rank))}`);
-  competitor.task_info[0].score_by_test.forEach((score, testId) => {
-    console.log(
-      `Score for test '${env.meta.tests[testId].name}': ${bold(blue(score))}`
-    );
+  console.log(`Team: ${bold(blue(teamName))}`);
+  console.log(`Total Score: ${bold(blue(totalScore))}`);
+  console.log(`Rank: ${bold(blue(rank))}`);
+  tests.forEach(({ name, score }) => {
+    console.log(`Score for test '${name}': ${bold(blue(score))}`);
   });
 }
--- src/hashCode.js
+++ src/hashCode.js
@@ -7,3 +7,25 @@ export function findCurrentChallenge(adventures) {
         startMs < Date.now() && Date.now() < endMs
     );
 }
+
+/**
+ *
+ * @param {*} scoreboard
+ * @param {string} competitorId
+ * @param {{ testNames: string[] }} options
+ */
+export function findScores(scoreboard, competitorId, { testNames }) {
+  const competitor = scoreboard.user_scores.find(
+    ({ competitor }) => competitor.id === competitorId
+  );
+  return {
+    teamName: competitor.displayname,
+    totalScore: competitor.score_1,
+    rank: competitor.rank,
+    tests: competitor.task_info[0].score_by_test.map((score, testId) => ({
+      id: testId,
+      name: testNames?.[testId],
+      score,
+    })),
+  };
+}
